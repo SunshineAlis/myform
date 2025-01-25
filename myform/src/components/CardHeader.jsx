150cf4c5bc934671262bf337fba148470127c71d
SunshineAlis <bumaam@yahoo.com>
2025-01-25 20:38:07 +0800

    1

--- myform/src/components/CardHeader.jsx
+++ myform/src/components/CardHeader.jsx
@@ -1,12 +1,11 @@
-export const CardHeader=()=>{
-    return (
+export const CardHeader = () => {
+  return (
     <>
-     <img src="/Pinecone.png" alt="Example" width={60} height={60} />
+      <img src="/Pinecone.png" alt="Example" width={60} height={60} />
       <h1 className="font-semibold text-xl">Join Us! 😎</h1>
       <h3 className="text-lg font-normal text-[#8E8E8E]">
         Please provide all current information accurately.
       </h3>
     </>
-    );
-  
+  );
 };
